0973c16d1faf660b35b8f76299c991c7a56f6bc1
sunnununii1 <134986553+sunnununii1@users.noreply.github.com>
2023-05-30 15:08:16 +0900

    자바 : 변수 기본형 및 선언 방법 예제

--- java/src/day1/variable/IntExample1.java
+++ java/src/day1/variable/IntExample1.java
@@ -3,14 +3,19 @@ package day1.variable;
 public class IntExample1 {
 
 	public static void main(String[] args) {
-		int num2 = 11;
-		int num3 = 0b11;
-		int num4 = 011;
-		int num5 = 0x11;
+		int num2 = 11;  // 십진수 11
+		int num3 = 0b11;   // 앞에 0b : 2진수 11 == 10진수 3
+		int num4 = 011;    // 앞에 0 : 8진수 11 == 10진수 9
+		int num5 = 0x11;   // 앞에 0x : 16진수 11 == 10진수 17
+		
 		System.out.println(num2);
 		System.out.println(num3);
 		System.out.println(num4);
 		System.out.println(num5);
+		
+		//1234567890123은 int로 표현할 수 없는 큰 숫자, 큰 숫자를 저장할 경우 끝에 l이나 L을 붙여야 함
+		long num6 = 1234567890123L;
+
 
 	}
 

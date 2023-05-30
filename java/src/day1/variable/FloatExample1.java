5d8d9403b51ba63293f443fc00c835e15324a7d5
sunnununii1 <134986553+sunnununii1@users.noreply.github.com>
2023-05-30 15:07:05 +0900

    자바 : 변수 기본형 및 선언 방법 예제

---
+++ java/src/day1/variable/FloatExample1.java
@@ -0,0 +1,13 @@
+package day1.variable;
+
+public class FloatExample1 {
+
+	public static void main(String[] args) {
+		double num1 = 3.14;
+		
+		//기본 실수 리터럴은(3.14와 같은) 타입이 double이다.
+		//double은 8바이트이기 때문에 float에 저장 불가능, 끝에 접미사 f나 F를 붙여야 함
+		float num2 = 3.14F;	
+	}
+
+}
---
+++ java/src/day1/variable/IntExample1.java
@@ -0,0 +1,17 @@
+package day1.variable;
+
+public class IntExample1 {
+
+	public static void main(String[] args) {
+		int num2 = 11;
+		int num3 = 0b11;
+		int num4 = 011;
+		int num5 = 0x11;
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(num5);
+
+	}
+
+}

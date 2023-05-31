7261fca05ecfa8ef15472e533a276c37925e8157
sunnununii1 <134986553+sunnununii1@users.noreply.github.com>
2023-05-31 15:14:57 +0900

    숙제 2

---
+++ java/src/day2/homework/Homework2.java
@@ -0,0 +1,14 @@
+package day2.homework;
+
+import java.util.Scanner;
+
+public class Homework2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in); 
+		
+		System.out.println("정수를 입력해주세요.");
+
+	}
+
+}

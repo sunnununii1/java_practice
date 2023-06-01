250ea143a062d186a918c44493df2e24d827f763
sunnununii1 <134986553+sunnununii1@users.noreply.github.com>
2023-06-01 15:15:23 +0900

    자바 3일차 숙제

--- java/src/day3/homework/H1.java
+++ java/src/day3/homework/H1.java
@@ -8,20 +8,26 @@ public class H1 {
 	public static void main(String[] args) {
 		// 성별(M:남성, W:여성)을 입력받아 여성인지 확인하는 코드를 작성하세요.
 		/* 조건연산자 이용.
-		 * 
-		 */
+		 *  M
+			Are you a women? false
+
+			input gender : 
+			W
+			Are you a women? true
+			* */
 
 		char ch;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("input gender : ");
 		ch = sc.next().charAt(0);
-		
+		String result = ch == 'w' ? "true" : "false";
+				
 		if(ch == 'w') { 
-			System.out.println("Are you women?");
+		System.out.println("Are you women?" + result);
 		}
-		
+				
 		if(ch == 'm') {
-			System.out.println("Are you men?");
+		System.out.println("Are you women?" + result);
 		}
 		
 	

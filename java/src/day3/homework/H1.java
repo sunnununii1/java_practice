3c0fbb811b586f380a01f826231b27f2495b40ef
sunnununii1 <134986553+sunnununii1@users.noreply.github.com>
2023-06-01 14:59:59 +0900

    자바 3일차 숙제 (여자인지)

--- java/src/day3/homework/H1.java
+++ java/src/day3/homework/H1.java
@@ -1,10 +1,31 @@
 package day3.homework;
 
+import java.util.Scanner;
+
 public class H1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 성별(M:남성, W:여성)을 입력받아 여성인지 확인하는 코드를 작성하세요.
+		/* 조건연산자 이용.
+		 * 
+		 */
 
+		int num;
+		Scanner sc = new Scanner(System.in);
+		num = nextInt();
+		System.out.println("input gender : ");
+		
+		if(num = 여자) { 
+			System.out.println("Are you women?");
+		}
+		
+		if(num = 남자) {
+			System.out.println("Are you men?");
+		}
+			
+	
+	sc. close();
+	
 	}
 
 }

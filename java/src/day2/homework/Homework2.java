3911f81e7184a0396bd0c96e3dd50a7158bd2405
sunnununii1 <134986553+sunnununii1@users.noreply.github.com>
2023-06-01 09:00:01 +0900

    숙제

--- java/src/day2/homework/Homework2.java
+++ java/src/day2/homework/Homework2.java
@@ -1,12 +1,30 @@
 package day2.homework;
 
+import java.util.Scanner;
+
 public class Homework2 {
 
 	public static void main(String[] args) {
-		int num1 = 1, num2 = 2;
 		
-		System.out.println(num1 + num2);
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("두 정수를 입력하세요.");
+		sc.nextLine();
+		String str1 = sc.nextLine();
+		System.out.println(str1);
+		
+		int num1 = 1, num2 = 2;
+		int sum = num1 + num2;
+		int sub = num1 - num2;
+		int mul = num1 * num2;
+		double div = (double)num1 / num2;
+		int mod = num1 % num2;
+		
+		System.out.println("" + num1 + "+" + num2 + "=" + sum);
+		System.out.println("" + num1 + "-" + num2 + "=" + sub);
+		System.out.println("" + num1 + "*" + num2 + "=" + mul);
+		System.out.println("" + num1 + "/" + num2 + "=" + div);
+		System.out.println("" + num1 + "%" + num2 + "=" + mod);
+		
 	}
 
 }

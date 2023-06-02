f5d8358bca80e6287a1a91228db4d316725e33d1
sunnununii1 <134986553+sunnununii1@users.noreply.github.com>
2023-06-02 14:36:38 +0900

    자바 : 중첩 if문 예제

---
+++ java/src/day4/ifex/NestedIfEx.java
@@ -0,0 +1,30 @@
+package day4.ifex;
+
+public class NestedIfEx {
+
+	public static void main(String[] args) {
+		/* 2,3,6의 배수 판별 예제
+		 * (day4.preactice 패키지에 IfMutipleEx2 문제)
+		 */
+		
+		int num = 6;
+		
+		//2의 배수
+		if(num % 2 == 0) {
+			if(num % 3 ==0) {
+				System.out.println("A myltiple of 6");
+			}
+		}
+		//3의 배수 ==> 6의 배수가 됨(6,12,18)
+		//3의 배수 가 아닌 수 ==> 2의 배수(2,4,6,8,10)
+		else if(num % 3 == 0) {
+			System.out.println("A nultiple of 3");
+		}
+		//아닌 수
+		else {
+			System.out.println("Not multiple of 2,3,6");
+		}
+		
+	}
+
+}

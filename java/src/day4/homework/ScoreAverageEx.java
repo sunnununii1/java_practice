41b476669597cc8364ca413279a3821e7fdf4bf0
sunnununii1 <134986553+sunnununii1@users.noreply.github.com>
2023-06-05 10:11:57 +0900

    자바 4일차 숙제 : 평균 구하기 문제풀이

--- java/src/day4/homework/ScoreAverageEx.java
+++ java/src/day4/homework/ScoreAverageEx.java
@@ -8,14 +8,19 @@ public class ScoreAverageEx {
 		// 국어, 영어, 수학 성적을 입력받아 평균을 출력하는 코드를 작성하세요.
 		
 		int kor, eng, math;
+		double avg; // ^
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("input score : ");
+		System.out.println("input score(kor, eng, math) ex : 100, 90, 80 : ");
 		kor = sc.nextInt();
 		eng = sc.nextInt();
 		math = sc.nextInt();
 		
-		System.out.println((kor + eng + math) / 3);
+		avg = (kor + eng + math) / 3.0; // 평균을 계산하는 식 필요^
+		/* 형변환 double을 따로 붙이지 않아도
+		 * 3을 3.0으로 바꾸면 더블이 된다
+		 */
+		System.out.println("avg : " + avg);
 		
 		
 	sc.close();

cfe827d0370f61094bd5c9f7f505f997007f54af
sunnununii1 <134986553+sunnununii1@users.noreply.github.com>
2023-06-05 14:59:59 +0900

    자바 5일차 : while문 1~10까지 증가 예제

---
+++ java/src/day5/whileex/CountNumberEx.java
@@ -0,0 +1,19 @@
+package day5.whileex;
+
+public class CountNumberEx {
+
+	public static void main(String[] args) {
+		/* 1부터 10까지 출력하는 코드를 작성하세요.
+		 * 반복횟수 : i는 1부터 i가 10까지 1씩 증가
+		 * 규칙성 : i를 출력
+		 */
+		
+		int i = 1; // i는 1부터
+		while(i <= 10) { // 10까지
+			System.out.println(i);
+			++i; // 1씩 증가
+		}
+	}
+
+}
+

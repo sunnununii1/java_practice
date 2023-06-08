7ef47b01ca6e6f5c9f8a5e5d364a2ae955f2d880
sunnununii1 <134986553+sunnununii1@users.noreply.github.com>
2023-06-09 08:57:36 +0900

    자바 7일차 과제 : 다이아몬드 별 찍기

--- java/src/day7/homework/StarEx5.java
+++ java/src/day7/homework/StarEx5.java
@@ -7,6 +7,7 @@ public class StarEx5 {
 		 *    ***       i=2  공=4    *=3
 		 *   *****      i=3  공=3    *=5
 		 *  *******     i=4  공=2    *=7
+		 *     +
 		 *   *****      i=5  공=3    *=5
 		 *    ***       i=6  공=4    *=3   
 		 *     *		i=7  공=5	*=1
@@ -14,20 +15,20 @@ public class StarEx5 {
 
 		for(int i=0 ; i<4 ; ++i) {
 		
-			for(int j=i ; j<4 ; ++j) {
+			for(int j=0 ; j<5-i; ++j) {
 				System.out.print(' ');
 			}
-			for(int j=0 ; j<=i*2+1 ; ++j) {
+			for(int j=0 ; j<i*2+1 ; ++j) {
 			System.out.print('*');
 			}
 			System.out.println();
 		}
 		
 		for(int i=0 ; i<3 ; i++) {
-			for(int j=0 ; j<3 ; ++j) {
+			for(int j=0 ; j<3+i ; ++j) {
 				System.out.print(' ');
 			}
-			for(int j=0 ; j<=(3-i)*2+1 ; ++j) {
+			for(int a=0 ; a<(3-i)*2-1 ; ++a) {
 				System.out.print('*');
 			}
 			System.out.println();

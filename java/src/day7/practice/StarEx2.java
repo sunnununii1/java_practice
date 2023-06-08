d79739e3ba970be5eec0fa69bd9d39bfa34d05f1
sunnununii1 <134986553+sunnununii1@users.noreply.github.com>
2023-06-08 14:43:47 +0900

    자바 7일차 : for문 직각삼각형 예제

---
+++ java/src/day7/practice/StarEx2.java
@@ -0,0 +1,35 @@
+package day7.practice;
+
+public class StarEx2 {
+
+	public static void main(String[] args) {
+		/* *       i=1  *=1
+		 * **      i=2  *=2
+		 * ***     i=3  *=3
+		 * ****    i=4  *=4
+		 * *****   i=5  *=5
+		 *              *=i
+		 * */ 
+		
+	
+		
+		/* 외부 반복문
+		 * 반복횟수 : i는 1부터 5까지 1씩 증가
+		 * 규칙성 : * 5개를 출력 후 엔터
+		 * */
+		for(int i=1 ; i<=5 ; ++i) {
+			
+			/* 내부 반복문
+			 * 반복횟수 : j는 1부터 i까지 1씩 증가
+			 * 규칙성 : *을 출력
+			 * */
+			for(int j=1 ; j<=i ; ++j) {
+				System.out.print("*");
+			}
+			System.out.println(); // 엔터
+		}
+	}
+	
+	
+}
+

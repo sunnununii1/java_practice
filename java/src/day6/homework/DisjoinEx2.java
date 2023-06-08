18aeda3d973714c82f979cad0fa6247dad09f532
sunnununii1 <134986553+sunnununii1@users.noreply.github.com>
2023-06-08 09:15:31 +0900

    자바 6일차 과제 : 서로소 예제(for문)

---
+++ java/src/day6/homework/DisjoinEx2.java
@@ -0,0 +1,50 @@
+package day6.homework;
+
+import java.util.Scanner;
+
+public class DisjoinEx2 {
+
+	public static void main(String[] args) {
+		// 두 정수를 입력받아 두 정수가 서로소 관계인지 판별하는 코드를 작성하세요.
+		/* 서로소 : 최대 공약수가 1인 두 수의 관계
+		 * input : 2 numbers : 3 4
+		 * Disjoint! (서로소)
+		 * input : 2 numbers : 8 4
+		 * Not Disjoint! (서로소 아님)
+		 * 
+		 * for문 이용
+		 */
+		
+		// day6.practice > GCDEx 참고!
+		 
+		
+		int num1, num2;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("input : ");
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		
+		int i = 1;
+		int gcd = 1;
+		for(i=1 ; i <= num1 ; ++i) {
+			if(num1 % i == 0 && num2 % i == 0) {
+			/*규칙성 : i가 num1의 약수이고, i가 num2의 약수이면 i를 gcd에 저장
+					->num1을 i로 나누었을 때 나머지가 0과 같고,
+		 		  	  num2를 i로 나누었을 때 나머지가 0과 같다면 
+		 	* */
+			gcd = i; // i를 gcd에 저장
+			}
+		}
+		
+		
+		// 반복문 종료 후 : gcd가 1이면 Disjoint! 출력, 아니면 Not Disjoint! 출력
+		if(gcd == 1) {
+			System.out.println("Disjoint!");
+		}
+		else {System.out.println("Not Disjoint!");
+		}
+		
+		sc.close();
+	}
+
+}

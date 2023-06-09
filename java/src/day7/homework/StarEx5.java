d41f12d60ca672bf82c8d1bd0c19d4b2dcf62174
sunnununii1 <134986553+sunnununii1@users.noreply.github.com>
2023-06-09 09:26:39 +0900

    자바 7일차 과제 : 다이아몬드 별 찍기 예제

--- java/src/day7/homework/StarEx5.java
+++ java/src/day7/homework/StarEx5.java
@@ -3,38 +3,46 @@ package day7.homework;
 public class StarEx5 {
 
 	public static void main(String[] args) {
-		/*     *        i=1  공=5    *=1
-		 *    ***       i=2  공=4    *=3
-		 *   *****      i=3  공=3    *=5
-		 *  *******     i=4  공=2    *=7
-		 *     +
-		 *   *****      i=5  공=3    *=5
-		 *    ***       i=6  공=4    *=3   
-		 *     *		i=7  공=5	*=1
-		 * */ 
+		/*    *        i=1   공=3     *=1
+		 *   ***       i=2   공=2     *=3
+		 *  *****      i=3   공=1     *=5
+		 * *******     i=4   공=0     *=7
+		 *    +			  공=half-i 	 *=2*i-1
+		 *  *****      i=5   공=1     *=5 (5는 2*3-1과 같다)
+		 *   ***       i=6   공=2     *=3 (3는 2*2-1과 같다)
+		 *    *		   i=7   공=3     *=1 (1는 2*1-1과 같다)
+		 *    			  공=i-half   *=2*(num+1-i)-1 (은 2*(num-i)+1과 같다)
+		 * */ 	
 
-		for(int i=0 ; i<4 ; ++i) {
 		
-			for(int j=0 ; j<5-i; ++j) {
-				System.out.print(' ');
+		//위 4줄부터 먼저 출력 => (num+1) / 2 
+		int num = 7;
+		for(int i=1 ; i<=num; ++i) {
+			int half = (num+1) / 2;
+			
+			if(i <= half ) {
+				//half-i개 공백 출력
+				for(int j =1; j<=half-i; j++) {
+					System.out.print(' ');
+				}
+				for(int j=1; j<=2*i-1; j++) {
+					System.out.print('*');
+				}
+				System.out.println();
 			}
-			for(int j=0 ; j<i*2+1 ; ++j) {
-			System.out.print('*');
+			//3줄
+			else {
+				//i-half개 공백 출력
+				for(int j =1; j<=i-half; j++) {
+					System.out.print(' ');
+				}
+				//2*(num-i)+1개 *출력
+				for(int j=1; j<=2*(num-i)+1; j++) {
+					System.out.print('*');
+				}
+				//엔터
+				System.out.println();
 			}
-			System.out.println();
 		}
-		
-		for(int i=0 ; i<3 ; i++) {
-			for(int j=0 ; j<3+i ; ++j) {
-				System.out.print(' ');
-			}
-			for(int a=0 ; a<(3-i)*2-1 ; ++a) {
-				System.out.print('*');
-			}
-			System.out.println();
-		}
-		
-		
 	}
-
 }

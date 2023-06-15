6733ae40f56946397f6ba4c71cc9ad637ddc9813
sunnununii1 <134986553+sunnununii1@users.noreply.github.com>
2023-06-15 10:59:46 +0900

    자바 11일차 과제 : 랜덤 야구게임 출력 예제

---
+++ java/src/array/Array.java
@@ -0,0 +1,92 @@
+package array;
+
+public class Array {
+	
+	public static void printArray(int arr[]) {
+		System.out.print("[");
+		for(int i=0 ; i<arr.length ; i++) {
+			System.out.print(arr[i] + (i==arr.length-1 ? "" : ","));
+		}
+		System.out.println("]");
+	}
+	
+	//위에꺼 오버로딩해서 아래로 수정한 것
+	//배열을 start번지부터 end-1번지까지 출력
+	public static void printArray(int arr[], int start, int end) {
+		System.out.print("[");
+		for(int i = start; i<end ; i++) {
+			if(i >= arr.length) {
+				break;
+			}
+			System.out.print(arr[i] + (i==end-1 ? "":", "));
+		}
+		System.out.print("]");
+	}
+	
+	//num가 배열의 원소 중 0번지부터 count개 안에 있는지 확인하는 메서드
+	public static boolean contains(int arr[], int num, int count) {
+		for(int i=0 ; i<count ; i++) {
+			if(num==arr[i]) {
+				return true;	//증복 발견시 break로 멈출 필요 없이, 바로 메서드 종료로 중복 알림
+			}
+		}
+		//여기까지 왔다는 건  return true를 못 만났다는 뜻 => 중복이 없었다는 뜻
+		return false; //true false 둘 다 가능 (에러방지용)
+	}
+	
+	public static int[] createRandomArray(int min, int max, int arr[]) {
+		if(max < min) {
+			//max가 min보다 작으면 두 수 교환
+			int tmp = max;
+			max = min;
+			min = max;
+		}
+		
+		//배열이 생성되어 있지 않으면 min과 max 사이의 랜덤한 수의 배열 생성
+		if(arr == null) {
+			arr = new int[max - min + 1];
+		}
+		
+		/*랜덤으로 만들어지는 수의 개수가 배열의 크기보다 작으면 메서드 종료
+		 * ex. 1~3이면 3개가 4개짜리 배열에 중복되지 않게 못들어감 */
+		if(max - min +1 < arr.length) {
+			return null;
+		}
+		
+		int count = 0;
+		while(count< arr.length) {
+			int random = (int)(Math.random()*(max-min+1) + min);
+			//중복되지 않았을 때, 배열에 저장 후 count 증가
+			if(!contains(arr,random,count)) {
+				arr[count] = random;
+				count++;
+			}
+		}
+		return arr;
+	}
+	/** 주어진 배열에 중복된 값이 있는지 없는지 알려주는 메서드
+	 * 매개변수 : 배열 = int arr[]
+	 * 리턴타입 : 중복 여부 => boolean
+	 * 메서드명 : arrayCheck
+	 */
+	public static boolean arrayCheck(int arr[]) {
+		if(arr == null) {
+			return false;
+		}
+		//arr 배열의 다른 번지들끼리 비교해서 같은 값이 있는지 확인하는 반복문
+		for(int i = 0 ; i<arr.length ; i++) {
+			for(int j =0 ; j<arr.length ; j++) {
+				//같은 번지일 땐 건너뜀
+				if(i == j) {
+					continue;
+				}
+				//다른 번지이고, 값이 같으면 중복되어 리턴
+				if(arr[i] == arr[j]) {
+					return true;
+				}
+			}
+		}
+		//반복문이 끝날 때까지 중복이 안나오면 안됐다고 리턴
+		return false;
+	}
+}
--- java/src/day11/homework/BaseballGameEx.java
+++ java/src/day11/homework/BaseballGameEx.java
@@ -2,14 +2,15 @@ package day11.homework;
 
 import java.util.Scanner;
 
+import array.Array;
+
 public class BaseballGameEx {
 
 	public static void main(String[] args) {
 		// 숫자 야구 게임을 구현하세요. 1~9 사이의 중복되지 않은 세 수를 맞추는 게임
-		/* 규칙 :
-		 *  B: 같은 숫자가 있지만 위치가 다름
-		 *  S: 같은 숫자가 있고 위치가 같음
-		 *  30: 일치하는 숫자가 하나도 없음
+		/* 규칙 : B: 같은 숫자가 있지만 위치가 다름
+		 * 		 S: 같은 숫자가 있고 위치가 같음
+		 *		 30: 일치하는 숫자가 하나도 없음
 		 *  
 		 *  예.	랜덤숫자: 1 9 5 
 		 * 		사용자 : 1 2 3
@@ -25,35 +26,130 @@ public class BaseballGameEx {
 		 * 			Good!
 		 */
 		
-		Scanner sc = new Scanner(System.in);
+		/* 풀이 순서
+		 * 1. 맞춰야 할 숫자 3개 랜덤 생성(중복X)
+		 * 2. 반복문 사용
+		 * 	 2-1. 사용자가 숫자 3개를 입력
+		 * 3. 판별하기
+		 */
 		
+		int com[] = new int[3];
+		int user[] = new int[3];
+		//1. 랜덤한 수를 저장할 배열과 사용자 입력 수 저장할 배열
 		int min = 1, max = 9;
 		
-		int[] random = new int[3];
-		int[] intput = new int[3];
+		int strike = 0; //스트라이크
+		int ball = 0; //볼의 갯수
+		Scanner sc = new Scanner(System.in);
 		
-		int S = 0;
-		int B = 0;
+		Array.createRandomArray(min, max, com);
+		//Array.printArray(com); //얜 나중에 주석처리
+		//1. 맞춰야할 숫자 3개 랜덤생성
 		
-		for(int i=1 ; i<=random.length ; i++) {
-			random[0] = (int)(Math.random() * (max-min+1) + min); 
-			random[1] = (int)(Math.random() * (max-min+1) + min); 
-			if(random[0] == random[1]) {
-					i--;
-					break;//같을시 전단계로+빠져나옴			
-				}
-			random[3] = (int)(Math.random() * (max-min+1) + min); 
-			if(random[0] == random[3]) {
-				i--;
-				break;			
+		//반드시 한번은 실행되는 반복문 == do
+		do {
+			//2-1. 사용자가 숫자 3개 입력
+			System.out.print("user : ");
+			for(int i = 0 ; i<user.length ; i++) {
+				user[i] = sc.nextInt();
+			}
+			//사용자 입력 체크
+			if(Array.arrayCheck(user)) {
+				System.out.println("You must not enter the same number!");
+				continue;
 			}
 			
-			
-			for(int j=0 ; j<i ; j++) {
-				
-			}
-		}
+			//3. 판별
+			//스트라이크 갯수 판별
+			strike = strike(com,user);
 	
+			//볼의 갯수 판별
+			ball = ball(com,user);
+	
+			//스트라이크와 볼의 갯수에 맞게 출력
+			printResult(strike,ball);
+
+		} while(strike < 3);
+		
+		System.out.println("Good!");
+		sc.close();
 	}
 
+	/** 스크라이크 개수를 판별하는 메서드
+	 * => 두 배열에서 같은 번지에 있는 값들이 몇개 같은지 알려주는 메서드
+	 * 매개변수 : 두 배열 => int arr1[], int arr2[]
+	 * 리턴타입 : 같은 번지에 있는 값들이 몇개 같은지 => 몇개 => 정수 => int
+	 * 메서드명 : strike 
+	 * */
+	public static int strike(int arr1[], int arr2[]) {
+		//배열 생성이 안됨, 배열이 하나라도 있으면 비교할 수 없어서 0으로 리턴
+		if(arr1 == null || arr2 == null) {
+			return 0;
+		}
+		//배열의 개수가 다른 경우 작은 크기의 배열의 기준으로 비교위해 size 계산
+		int size = arr1.length < arr2.length ? arr1.length : arr2.length;
+		
+		int count = 0; //스트라이크 개수
+		for(int i = 0 ; i<size; i++) {
+			//같은 번지에 있는 값들이 서로 같을시(스트라이크일 때)
+			if(arr1[i] == arr2[i]) {
+				count++;
+			}
+		}
+		return count;
+	}
+	/** 볼의 개수를 판별하는 메서드
+	 * => 두 배열에서 다른 번지에 있는 값들이 몇개 같은지 알려주는 메서드
+	 * 매개변수 : 두 배열 => int arr1[], int arr2[]
+	 * 리턴타입 : 다른 번지에 있는 값들이 몇개 같은지 => 몇개 => 정수 => int
+	 * 메서드명 : strike 
+	 * */
+	public static int ball(int arr1[], int arr2[]) {
+		//배열 생성이 안됨, 배열이 하나라도 있으면 비교할 수 없어서 0으로 리턴
+		if(arr1 == null || arr2 == null) {
+			return 0;
+		}
+		
+		//방법 1.
+		int count = 0;
+		//다른 번지를 비교해서 찾아도 되나, 두 배열에서 같은 숫자의 개수에서 스트라이크 갯수 뺴면 볼 개수가 됨
+		for(int tmp :arr1) {
+			//배열1에서 꺼낸 값이 배열2에 있으면 개수를 증가 
+			if(Array.contains(arr2, tmp, arr2.length)) {
+				count++;
+			}
+		}
+		return count - strike(arr1, arr2);
+		
+		//방법2. 이중 반복문 이용해서 다른 번지 비교하는 코드
+		/*for(int i = 0 ; i<arr1.length ; i++) {
+			for(int j = 0; j<arr2.length ; j++) {
+				if(i ==j) {
+					continue;
+				}
+				if(arr1[i] == arr2[i]) {
+					count++;
+				}
+			}
+		}
+		return count;
+		*/
+	}
+	/** 스트라이크와 볼의 개수가 주어지면 결과를 출력하는 메서드
+	 * 매개변수 : 스트라이크 개수, 볼의 개수 => int strike, int ball
+	 * 리턴타입 : 출력이라 없음 => void
+	 * 메서드명 : printResult
+	 */
+	public static void printResult(int strike, int ball) {
+		if(strike != 0) {
+			System.out.print(strike + "S");
+		}
+		if(ball != 0) {
+			System.out.print(ball + "B");
+		}
+		if(strike == 0 && ball == 0) {
+			System.out.print("3O");
+		}
+		System.out.println();
+	}
 }

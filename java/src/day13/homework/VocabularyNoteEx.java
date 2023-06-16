999860dad3003503639247c3f8dc6ca5be398fb0
sunnununii1 <134986553+sunnununii1@users.noreply.github.com>
2023-06-16 12:36:53 +0900

    자바 13일차 과제

---
+++ java/src/day13/homework/VocabularyNoteEx.java
@@ -0,0 +1,27 @@
+package day13.homework;
+
+public class VocabularyNoteEx {
+
+	public static void main(String[] args) {
+		// 영어 단어장 프로그램 구현하세요.
+		/* 메뉴
+		 * 1. 단어 추가
+		 * 2. 단어 삭제
+		 * 3. 단어 수정
+		 * 		a. 단어 수정
+		 * 		b. 뜻 수정
+		 * 		c. 뜻 삭제
+		 * 4. 단어 출력
+		 * 		a. 전체 출력
+		 * 		b. 검색
+		 * 5. 프로그램 종료
+		 */
+		
+		/* 예. 메뉴 [엔터] 1~5번 출력
+		 * 	  메뉴 선택 : [입력]
+		 * 	  서브 메뉴 [엔터] a~c 출력
+		 */
+
+	}
+
+}

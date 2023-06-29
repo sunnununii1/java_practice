5024bf0d6d88d04c7ad350852f9e40775b569825
sunnununii1 <134986553+sunnununii1@users.noreply.github.com>
2023-06-29 14:58:43 +0900

    자바 22일차 과제

---
+++ java/src/day22/homework/run/Manager.java
@@ -0,0 +1,18 @@
+package day22.homework.run;
+
+public class Manager {
+
+	public static void main(String[] args) {
+		// 고등학생 관리 프로그램 구현
+		/* 학생 관리(입학,전학(오는 학생, 가는 학생), 졸업, 진학)
+		 * 성적 관리
+		 * 면담
+		 * 
+		 * 기능 구체적 정리(ex.요구사항 명세서)
+		 * 필요 클래스 추가(vo에)
+		 * 기능 하나씩 구현
+		 * */
+
+	}
+
+}
---
+++ java/src/day22/homework/vo/Student.java
@@ -0,0 +1,5 @@
+package day22.homework.vo;
+
+public class Student {
+
+}

150978c922c65c2d20f3bb87ad97e3f0d25813dc
sunnununii1 <134986553+sunnununii1@users.noreply.github.com>
2023-07-03 15:05:51 +0900

    자바 : 1대1 채팅 예제

---
+++ java/src/day24/network/ClientEx3.java
@@ -0,0 +1,25 @@
+package day24.network;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+
+public class ClientEx3 {
+	
+	public static void main(String[] args) {
+		int port = 5001; //1. 포트번호 설정
+		String ip = "192.168.30.212"; //1-1. 서버 아이피 설정
+		Socket socket = new Socket(); //2. 소켓 생성
+		try{
+			//3. 서버(OP,posrt)에게 연결 요청
+			socket.connect(new InetSocketAddress(ip,port));
+			//연결 완료되면 읽기/쓰기 기능 실행하기 - 서킷이 연결 끊길 때까지
+			Client client = new Client(socket);
+			client.read();
+			client.send();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}

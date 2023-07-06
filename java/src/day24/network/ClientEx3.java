a00d0d1aa1897f5e4c4fb65e2f6507b922bbe6e7
sunnununii1 <134986553+sunnununii1@users.noreply.github.com>
2023-07-06 16:44:48 +0900

    자바 24일차 : 소켓 프로그래밍 예제(서버 ip 설정)

--- java/src/day24/network/ClientEx3.java
+++ java/src/day24/network/ClientEx3.java
@@ -1,6 +1,7 @@
 package day24.network;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
@@ -10,6 +11,7 @@ public class ClientEx3 {
 	public static void main(String[] args) {
 		int port = 5001; //1. 포트번호 설정
 		String ip = "192.168.30.212"; //1-1. 서버 아이피 설정
+		//ip = InetAddress.getLocalHost().getHostAddress(); //윗줄과의 차이 알기!
 		Socket socket = new Socket(); //2. 소켓 생성
 		try{
 			//3. 서버(OP,posrt)에게 연결 요청
